8f7ae90b5c1586484a619c6048e3929c1ab503e3
Luca Battesini <lucabattesini1@gmail.com>
2024-02-15 13:55:46 -0300

    Impproving dessign

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-function mudarPosicao() {
-    var botao = document.getElementById("nao-aceita");
-    var novaPosicaoX = Math.floor(Math.random() * window.innerWidth);
-    var novaPosicaoY = Math.floor(Math.random() * window.innerHeight);
-  
-    botao.style.left = novaPosicaoX + "px";
-    botao.style.top = novaPosicaoY + "px";
-}
+const botao = document.getElementById("nao-aceita")
+
+document.getElementById('nao-aceitou-botao').addEventListener('mouseover', () => {
+    const randomX = Math.random() * (window.innerWidth - botao.offsetWidth);
+    const randomY = Math.random() * (window.innerHeight - botao.offsetHeight);
+    
+    botao.style.left = randomX + 'px';
+    botao.style.top = randomY + 'px';
+  });
